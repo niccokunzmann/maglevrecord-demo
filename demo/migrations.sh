a7143bffe3b612c67c066ed8fcee1264b356714a
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-05-24 15:33:39 +0200

    added migration watcher

--- demo/migrations.sh
+++ demo/migrations.sh
@@ -2,17 +2,16 @@
 
 export MAGLEV_OPTS=-W0
 
-tempfile="`dirname $0`/../tmp/migrate_auto?"
+tempfile="`dirname $0`/../tmp/migrate_up?"
 
 while maglev-ruby -e "" :
 do
-  bundle exec rake migrate:auto? 1> $tempfile 2> $tempfile
+  bundle exec rake migrate:up? 1> $tempfile 2> $tempfile
   clear
   cat $tempfile
   echo ----------------------------
-  bundle exec rake migrate:auto? 1> $tempfile 2> $tempfile
+  bundle exec rake migrate:up? 1> $tempfile 2> $tempfile
   clear
   cat $tempfile
   echo ============================
 done
-
